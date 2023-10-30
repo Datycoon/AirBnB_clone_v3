c0d6cee01ca7511104478ed1900778bd6b1587ba
Datycoon <escowebs@gmail.com>
2023-10-30 17:28:15 +0000

    init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,11 +4,12 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
+from api.v1.views.index import index
+from api.v1.views.states import get_states, get_state
+from api.v1.views.cities import get_cities, get_city
+from api.v1.views.amenities import get_amenities, get_amenity
+from api.v1.views.users import get_users, get_user
+from api.v1.views.places import get_places, get_place
+from api.v1.views.places_reviews import get_place_reviews, create_place_review
+from api.v1.views.places_amenities import get_place_amenities, create_place_amenity
+
